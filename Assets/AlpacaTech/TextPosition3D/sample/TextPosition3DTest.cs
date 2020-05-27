f6959e7d2f314162a1a06d52c74b381154de56fb
uchida <uchida@sensetime.jp>
2020-05-27 15:11:51 +0900

    add 3D to 2D sample

---
+++ Assets/AlpacaTech/TextPosition3D/sample/TextPosition3DTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextPosition3DTest : MonoBehaviour {
+
+    [SerializeField] Canvas canvas;
+    [SerializeField] GameObject prefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+
+    Vector3 mousePositionBack;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var ix = Input.GetAxis("Horizontal");
+        var iy = Input.GetAxis("Vertical");
+
+        var mousePosition = Input.mousePosition;
+        if (Input.GetMouseButton(0))
+        {
+            var sub = mousePositionBack - mousePosition;
+            Camera.main.transform.position += sub * 0.1f;
+        }
+
+        this.transform.position += new Vector3(ix, iy, 0) * Time.deltaTime * 3.0f;
+        if (Input.GetMouseButtonDown(1))
+        {
+            var obj = Instantiate(prefab, canvas.transform);
+            var tp3 = obj.GetComponent<TextPosition3D>();
+            tp3.targetPosition = this.gameObject.transform.position;
+            Destroy(obj, 1f);
+        }
+
+        mousePositionBack = mousePosition;
+	}
+}
---
+++ Assets/AlpacaTech/TextPosition3D/script/TextPosition3D.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextPosition3D : MonoBehaviour
+{
+    //  追跡位置
+    public Vector3 offset = Vector3.zero;
+    public Vector3 targetPosition;
+    public Transform target;
+
+    private RectTransform rectTransform;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rectTransform = GetComponent<RectTransform>();
+	}
+
+    void Update()
+    {
+        if (target)
+        {
+            targetPosition = target.position;
+        }
+
+        var pos = targetPosition;
+        pos += Camera.main.transform.rotation * offset;
+        rectTransform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, pos);
+    }
+
+    private void OnValidate()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        Update();
+    }
+}
